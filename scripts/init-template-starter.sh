a8151deb8e76b702aed74fb5c3b2ee435805e8c5
Joe Corall <jjc223@lehigh.edu>
2024-05-11 12:59:43 -0400

    Get site to come online in CI (#1)

--- scripts/init-template-starter.sh
+++ scripts/init-template-starter.sh
@@ -10,13 +10,15 @@ if [ ! -v ISLANDORA_STARTER_REF ] || [ "$ISLANDORA_STARTER_REF" = "" ]; then
   ISLANDORA_STARTER_REF=heads/main
 fi
 
+if [ ! -v ISLANDORA_TAG ] || [ "$ISLANDORA_TAG" = "" ]; then
+  ISLANDORA_TAG=main
+fi
+
 mkdir -p isle-site-template
 pushd "$(pwd)/isle-site-template"
 
 curl -L "https://github.com/Islandora-Devops/isle-site-template/archive/refs/${ISLE_SITE_TEMPLATE_REF}.tar.gz" \
   | tar -xz --strip-components=1
-rm -rf .github setup.sh
-
 mv drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt .
 
 curl -L "https://github.com/Islandora-Devops/islandora-starter-site/archive/refs/${ISLANDORA_STARTER_REF}.tar.gz" \
@@ -24,7 +26,6 @@ curl -L "https://github.com/Islandora-Devops/islandora-starter-site/archive/refs
 
 mv default_settings.txt drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt
 
-rm -rf certs/* secrets/*
 ./generate-certs.sh
 ./generate-secrets.sh
 
