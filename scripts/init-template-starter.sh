e3fe4e911ea95cbea518ef049ed121ca78eb0d58
Joe Corall <jjc223@lehigh.edu>
2024-05-11 11:19:32 -0400

    Debug site template bash on ubuntu: I think head -74 is causing grep to write to a closed pipe

--- scripts/init-template-starter.sh
+++ scripts/init-template-starter.sh
@@ -25,6 +25,19 @@ curl -L "https://github.com/Islandora-Devops/islandora-starter-site/archive/refs
 mv default_settings.txt drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt
 
 rm -rf certs/* secrets/*
+
+readonly CHARACTERS='[A-Za-z0-9]'
+readonly LENGTH=32
+
+SALT_FILE=salt
+secret=ok
+(grep -ao '[A-Za-z0-9_-]' </dev/urandom || true) | head -74 | tr -d '\n' >"${SALT_FILE}"
+
+(grep -ao "${CHARACTERS}" </dev/urandom || true) | head "-${LENGTH}" | tr -d '\n' >"${secret}"
+
+cat "$SALT_FILE"
+cat "$secret"
+
 ./generate-certs.sh
 ./generate-secrets.sh
 
