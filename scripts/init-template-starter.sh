4d898cbd8443180e770e11a24ac0127f20c65df2
Joe Corall <jjc223@lehigh.edu>
2024-05-11 12:37:18 -0400

    Allow cURL to fail in ping

--- scripts/init-template-starter.sh
+++ scripts/init-template-starter.sh
@@ -19,16 +19,12 @@ pushd "$(pwd)/isle-site-template"
 
 curl -L "https://github.com/Islandora-Devops/isle-site-template/archive/refs/${ISLE_SITE_TEMPLATE_REF}.tar.gz" \
   | tar -xz --strip-components=1
-rm -rf .github setup.sh
-
 mv drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt .
 
 curl -L "https://github.com/Islandora-Devops/islandora-starter-site/archive/refs/${ISLANDORA_STARTER_REF}.tar.gz" \
   | tar --strip-components=1 -C drupal/rootfs/var/www/drupal -xz
 
 mv default_settings.txt drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt
-
-rm -rf certs/* secrets/*
 
 ./generate-certs.sh
 ./generate-secrets.sh
--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
-
 COUNTER=0
 while true; do
   HTTP_STATUS=$(curl -w '%{http_code}' -o /dev/null -s https://islandora.dev/)
+  echo "Ping returned http status ${HTTP_STATUS}, exit code $?"
   if [ "${HTTP_STATUS}" -eq 200 ]; then
     echo "We're live 🚀"
     exit 0
   fi
-  echo "Ping returned ${HTTP_STATUS}"
+
   ((COUNTER++))
   if [ "${COUNTER}" -eq 50 ]; then
     echo "Failed to come online after 4m"
